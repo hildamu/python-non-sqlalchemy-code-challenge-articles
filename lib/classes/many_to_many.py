1c7de521a1ce3b27670495104b977c95e846a770
hildamu <mukamihilda437@gmail.com>
2024-05-28 11:21:16 +0300

    submit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -24,6 +24,28 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+    
+    name=property(get_name,set_name)
+
+    def get_category(self):
+        return self._category
+    
+    def set_category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        
+    category=property(get_category,set_category)
+
+    def add_article(self, article):
+        self._articles.append(article)
 
     def articles(self):
         pass
